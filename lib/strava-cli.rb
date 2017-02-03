7b1f7d54e84db59066d1403c916d9aa50e30ed15
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2017-02-03 19:54:21 +0100

    add options parse 👏🏽

--- lib/strava-cli.rb
+++ lib/strava-cli.rb
@@ -1,4 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'optparse'
+
 require_relative 'strava-cli/command'
--- lib/strava-cli/command.rb
+++ lib/strava-cli/command.rb
@@ -1,7 +1,37 @@
 module StravaCli
   class Command
+    Error = StandardError
+
+    def initialize(argv = [])
+      @argv = argv
+    end
+
     def run
+      parse(argv)
       puts "strava-cli"
+      puts "type: #{type}"
+      puts "duration: #{duration}"
+      puts "time: #{time}"
+    rescue Error => error
+      puts error.message
+    end
+
+    private
+    attr_reader :argv
+    attr_reader :type, :time, :duration
+
+    def parse(argv)
+      command = argv[0]
+      raise Error.new("Unsuported command: #{command}") unless command == "add"
+      @type = argv[1]
+      OptionParser.new(argv) do |parser|
+        parser.on("-t", "--time TIME", "Date of the activity") do |v|
+          @time = v
+        end
+        parser.on("-d", "--duration DURATION", "Duration of the activity") do |v|
+          @duration = v
+        end
+      end.parse!
     end
   end
 end
